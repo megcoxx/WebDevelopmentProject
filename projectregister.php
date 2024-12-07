69e179186ccacd61d9b2529b2875178e2460b017
megcoxx <mcox6232@gmail.com>
2024-12-07 18:35:18 -0500

    changed pdf to script file

--- projectregister.php
+++ projectregister.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "task_manager");
+$conn = new mysqli("localhost", "root", "newpassword", "task_manager");
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $username = $_POST["username"];
